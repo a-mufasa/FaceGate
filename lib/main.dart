b9b19dc2295ae8e879632b692ef1596f82503189
Justin Kilgo <justin.kilgo314@gmail.com>
2022-11-12 04:34:53 -0600

    setup DB communication

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'register_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
