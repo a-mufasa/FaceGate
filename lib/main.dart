2fd31ea9be194b756cffaffe2da95911cfe3c58a
Rohit Kala <kalarohit9@gmail.com>
2022-11-12 01:21:03 -0600

    Basic structure of register_view done

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return register_view();
-}
+    final app_title = "Facegate";
+    return MaterialApp(title: app_title, home: RegisterView());
+  }
 }
--- lib/register_view.dart
+++ lib/register_view.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
+import 'form_view.dart';
 
-class register_view extends StatelessWidget{
-
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to Flutter'),
-          ),
-          body: Center(
-            child: Text("Hello Worlds!"),
-          )),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Facegate'),
+        ),
+        body: const Align(
+          child: RegisterButton(),
+        ));
   }
+}
 
+class RegisterButton extends StatefulWidget {
+  const RegisterButton({super.key});
 
+  @override
+  State<RegisterButton> createState() => _RegisterButtonState();
+}
+
+class _RegisterButtonState extends State<RegisterButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Image(
+        image: const AssetImage('assets/temp_logo.png'),
+        width: 120,
+        height: 75,
+      ),
+      Padding(padding: EdgeInsets.all(10)),
+      TextButton(
+          onPressed: () {
+            final newRoute =
+                MaterialPageRoute(builder: (context) => const FormView());
+            Navigator.push(context, newRoute);
+          },
+          child: const Text("Register"))
+    ]);
+  }
 }
